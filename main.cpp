f5d5972a6dc8d09ddfc72c66d32723ad5054ae7d
Joshua Bays <joshua_bays@protonmail.com>
2021-07-27 08:09:17 +0000

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -15,19 +15,19 @@ std::deque<std::string> parse_line(std::string line); //Splits a string into ind
 std::string sanitize_string(std::string line); //Removes all punctuation from a string and makes it entirely lowercase
 
 int main(int argc, char *argv[]){
-	if(argc != 4){
+	if(argc != 4){ //3 Arguments: input file, minimum frequency, maximum frequency
 		printf("Too many or too few arguments, exiting.\n"); return 1;
 	}
-	std::string sourceFile = argv[1];
-	int min = std::stoi(argv[2]); int max = std::stoi(argv[3]);
-	std::ifstream readFile; readFile.open(sourceFile); std::string line;
+	std::string sourceFile = argv[1]; //Argument 1: input file
+	int min = std::stoi(argv[2]); int max = std::stoi(argv[3]); //Arguments 2 and 3: minimum and maximum frequencies
 	
+	std::ifstream readFile; readFile.open(sourceFile); std::string line;
 	std::deque<std::string> parsedLine;
 	std::deque<struct wordFreq> wordDeque; int wordDequeIndex;
 	
 	while(getline(readFile, line)){
-		parsedLine = parse_line(sanitize_string(line.substr(line.find("|"), line.size() - 1)));
-		for(std::string &i : parsedLine){
+		parsedLine = parse_line(sanitize_string(line.substr(line.find("|"), line.size() - 1))); //Parse a line of the document
+		for(std::string &i : parsedLine){ ///Check if each word in the parsed line has already been discovered in the source file, if it has, increase its frequency by 1, otherwise, add it too the list
 			wordDequeIndex = find_wordDeque_index(wordDeque, i);
 			if(wordDequeIndex == -1){
 				wordDeque.push_back({i, 1});
@@ -38,7 +38,7 @@ int main(int argc, char *argv[]){
 	}
 	
 	std::deque<std::string> refs;
-	for(struct wordFreq &i : wordDeque){
+	for(struct wordFreq &i : wordDeque){ //For each word, if its frequency is between a specified range, find its corresponding references
 		if(i.frequency >= min && i.frequency <= max){
 			printf("%s|", i.word.c_str());
 			refs = find_references_for_word(i.word, sourceFile);
@@ -48,7 +48,7 @@ int main(int argc, char *argv[]){
 			printf("%s\n", refs[refs.size() - 1].c_str());
 		}
 	}
-
+	
 	return 0;
 }
 
@@ -67,7 +67,7 @@ int find_wordDeque_index(std::deque<struct wordFreq> wordDeque, std::string word
 std::deque<std::string> find_references_for_word(std::string word, std::string sourceFile){
 	std::deque<std::string> ret; std::deque<std::string> parsedLine;
 	std::ifstream openFile; openFile.open(sourceFile); std::string line;
-	while(getline(openFile, line)){
+	while(getline(openFile, line)){ //For each word in each line of the source file, check it against the passed word and add the reference if it is a match
 		parsedLine = parse_line(sanitize_string(line.substr(line.find("|") + 1, line.size() - 1)));
 		for(std::string &i : parsedLine){
 			if(i == word){
@@ -81,7 +81,7 @@ std::deque<std::string> find_references_for_word(std::string word, std::string s
 std::deque<std::string> parse_line(std::string line){
 	std::deque<std::string> ret;
 	int wordStart;
-	while(1){
+	while(1){ //Separate the words by finding the spaces between them
 		wordStart = line.find(" ");
 		if(wordStart > 0){
 			ret.push_back(line.substr(0, wordStart));
@@ -98,10 +98,10 @@ std::string sanitize_string(std::string line){
 	std::string ret = "";
 	for(char &i : line){
 		switch(i){
-			case 'A' ... 'Z':
+			case 'A' ... 'Z': //Make uppercase characters lowercase
 				ret += i + 32;
 				break;
-			case ' ':
+			case ' ': //Include spaces, apostrophes, and lowercase characters
 			case '\'':
 			case 'a' ... 'z':
 				ret += i;
